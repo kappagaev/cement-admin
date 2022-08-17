bc617160dc53109a5d4386c49a6fa56a73cae4ec
kkpagaev <kapagaev@gmail.com>
2022-08-17 06:53:33 +0300

    refresh token fix

--- src/api/cement.ts
+++ src/api/cement.ts
@@ -7,6 +7,44 @@ export const cementApi = axios.create({
     'Content-Type': 'application/json',
   },
 })
+cementApi.interceptors.request.use((config: AxiosRequestConfig) => {
+  // if Authorization header is not set, set it
+  config.headers = { ...config.headers }
+  if (!config.headers.Authorization) {
+    config.headers.Authorization = `Bearer ${localStorage.getItem('accessToken')}`
+  }
+
+  return config
+})
+cementApi.interceptors.response.use(
+  (response) => {
+    return response
+  },
+  async function (error) {
+    const originalRequest = error.config
+
+    if (
+      error.response.status === 401 &&
+      !originalRequest._retry &&
+      !error.request.responseURL.endsWith('api/auth/refresh')
+    ) {
+      originalRequest._retry = true
+      const refreshToken = localStorage.getItem('refreshToken')
+      if (!refreshToken) {
+        return Promise.reject(error)
+      }
+      const refreshedTokens = await refreshAccessToken(refreshToken)
+      if (!refreshedTokens) {
+        return Promise.reject(error)
+      }
+      localStorage.setItem('accessToken', refreshedTokens.accessToken)
+      localStorage.setItem('refreshToken', refreshedTokens.refreshToken)
+      originalRequest.headers.Authorization = `Bearer ${refreshedTokens.accessToken}`
+      return cementApi(originalRequest)
+    }
+    return Promise.reject(error)
+  },
+)
 
 // cementApi.interceptors.request.use((config: AxiosRequestConfig) => {
 //   config.headers = { ...config.headers }
--- src/context/AxiosProvider.tsx
+++ src/context/AxiosProvider.tsx
@@ -16,45 +16,6 @@ export const useAxios = () => {
 export const AxiosProvider = ({ children }: { children: ReactNode | ReactNode[] }) => {
   const [cement, setCement] = React.useState<AxiosInstance>(cementApi)
   const { tokens, setTokens, logout, isLogged } = useAuth()
-  useEffect(() => {
-    if (tokens.accessToken && isLogged) {
-      cementApi.interceptors.request.use((config: AxiosRequestConfig) => {
-        // if Authorization header is not set, set it
-        config.headers = { ...config.headers }
-        if (!config.headers.Authorization) {
-          config.headers.Authorization = `Bearer ${tokens.accessToken}`
-        }
 
-        return config
-      })
-      cementApi.interceptors.response.use(
-        (response) => {
-          return response
-        },
-        async function (error) {
-          const originalRequest = error.config
-
-          if (
-            error.response.status === 401 &&
-            !originalRequest._retry &&
-            !error.request.responseURL.endsWith('api/auth/refresh')
-          ) {
-            originalRequest._retry = true
-            const refreshedTokens = await refreshAccessToken(tokens.refreshToken)
-            if (!refreshedTokens) {
-              logout()
-              return Promise.reject(error)
-            }
-            setTokens(refreshedTokens)
-            return cement(originalRequest)
-          }
-          return Promise.reject(error)
-        },
-      )
-    }
-    // refreshesh token if it is expired
-
-    setCement(cementApi)
-  }, [tokens])
   return <axiosContext.Provider value={{ cement }}>{children}</axiosContext.Provider>
 }
